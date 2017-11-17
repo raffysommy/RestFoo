2665e6f6f03e76ae1a582845183eaed331d9819f
Raffaele <raffysommy@gmail.com>
2017-11-17 12:38:38 +0100

    Ri aggiunta validazione

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.xml.sax.SAXException;
 
 import com.microsoft.z3.Context;
 
@@ -19,9 +25,11 @@ public class Main {
 		try {
             // create a JAXBContext capable of handling the generated classes
             JAXBContext jc = JAXBContext.newInstance( "it.polito.verifoo.rest.jaxb" );
-            
             // create an Unmarshaller
             Unmarshaller u = jc.createUnmarshaller();
+            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI); 
+            Schema schema = sf.newSchema( new File( "./xsd/nfvInfo.xsd" )); 
+            u.setSchema(schema);
             // unmarshal a document into a tree of Java content objects
             NFV root = (NFV) u.unmarshal( new FileInputStream( "./xsd/nfvInfo.xml" ) );
 			// make changes here to the root
@@ -52,6 +60,9 @@ public class Main {
 			System.out.println("NFFG semantically incorrect");
         	e.printStackTrace();
             System.exit(1);
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
